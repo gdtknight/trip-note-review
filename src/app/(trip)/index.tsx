a06a4d16c10b1ecff3fc3fd416ad8d58daa1b09a
yoshin <yoshin@student.42gyeongsan.kr>
2026-09-26 01:07:22 +0900

    [#2] [5. 여행 리스트 api 연동] 여행 리스트 화면 구현

--- src/app/(trip)/index.tsx
+++ src/app/(trip)/index.tsx
@@ -1,20 +1,28 @@
 import PlusButton from "@/components/PlusButton";
 import TripCard from "@/components/TripCard";
 import { theme } from "@/constants/theme";
+import { useGetTripList } from "@/hooks/useTrip";
 import { useRouter } from "expo-router";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const MyTripList = () => {
   const router = useRouter();
+  const { data } = useGetTripList();
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>내 여행</Text>
       <ScrollView contentContainerStyle={{ gap: 10 }}>
-        <TripCard />
-        <TripCard />
-        <TripCard />
+        {data?.data.map((item) => (
+          <TripCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            startDate={item.startDate}
+            endDate={item.endDate}
+          />
+        ))}
       </ScrollView>
       <View style={styles.buttonContainer}>
         <PlusButton onPress={() => router.navigate("/createTrip")} />
